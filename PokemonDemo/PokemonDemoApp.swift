4231508b22592c2bda81b1c4c7b07bd8bed456f9
trey03 <adixon1@unc.edu>
2024-05-28 00:09:04 -0400

    Initial Commit

---
+++ PokemonDemo/PokemonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDemoApp.swift
+//  PokemonDemo
+//
+//  Created by Trey Dixon on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
